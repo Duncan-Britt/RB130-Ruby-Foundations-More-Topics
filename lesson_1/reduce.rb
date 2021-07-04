8f753cb1440b740c2ad71bb4d7bb6399f9638956
Duncan Britt <dbru997@gmail.com>
2021-07-04 13:51:30 -0600

    reduce extra features implemented

--- lesson_1/reduce.rb
+++ lesson_1/reduce.rb
@@ -1,5 +1,11 @@
-def reduce(arr, acc=0)
-  idx = 0
+def reduce(arr, accumulator=nil)
+  if accumulator
+    acc = accumulator
+    idx = 0
+  else
+    acc = arr[0]
+    idx = 1
+  end
   while idx < arr.size
     acc = yield(acc, arr[idx])
     idx += 1
@@ -11,4 +17,6 @@ array = [1, 2, 3, 4, 5]
 
 p reduce(array) { |acc, num| acc + num }                    # => 15
 p reduce(array, 10) { |acc, num| acc + num }                # => 25
-reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+# reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+p reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
+p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
