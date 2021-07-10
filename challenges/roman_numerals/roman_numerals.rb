7f070f0a5f3f6b404018f40fcbf3a81ae77321b5
Duncan Britt <dbru997@gmail.com>
2021-07-10 13:59:56 -0600

    Roman numeral converter implemented

---
+++ challenges/roman_numerals/roman_numerals.rb
@@ -0,0 +1,45 @@
+class RomanNumeral
+  NUMERAL = {
+    1 => 'I',
+    2 => 'II',
+    3 => 'III',
+    4 => 'IV',
+    5 => 'V',
+    6 => 'VI',
+    7 => 'VII',
+    8 => 'VIII',
+    9 => 'IX',
+    10 => 'X',
+    20 => 'XX',
+    30 => 'XXX',
+    40 => 'XL',
+    50 => 'L',
+    60 => 'LX',
+    70 => 'LXX',
+    80 => 'LXXX',
+    90 => 'XC',
+    100 => 'C',
+    200 => 'CC',
+    300 => 'CCC',
+    400 => 'CD',
+    500 => 'D',
+    600 => 'DC',
+    700 => 'DCC',
+    800 => 'DCCC',
+    900 => 'CM',
+    1000 => 'M',
+    2000 => 'MM',
+    3000 => 'MMM'
+  }
+  def initialize(decimal)
+    @decimal = decimal
+  end
+
+  def to_roman
+    d = @decimal.digits
+    d.map!.with_index { |n, i| n * 10**i }.reverse!
+    d.map do |n|
+      NUMERAL[n]
+    end.join
+  end
+end
